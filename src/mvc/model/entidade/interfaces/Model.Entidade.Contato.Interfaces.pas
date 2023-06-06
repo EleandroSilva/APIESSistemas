f2b73c3e141cc377af5e8b17625d61b65d29ef9c
EleandroSilva <eleandrobemoreweb@gmail.com>
2023-06-06 12:07:09 -0300

    Update Model.Entidade.Contato.Interfaces.pas

--- src/mvc/model/entidade/interfaces/Model.Entidade.Contato.Interfaces.pas
+++ src/mvc/model/entidade/interfaces/Model.Entidade.Contato.Interfaces.pas
@@ -1,7 +1,7 @@
 {*******************************************************}
 {                    API PDV - JSON                     }
 {                      ES Sistemas                      }
-{            Início do projeto 27/05/2023               }
+{            InÃ­cio do projeto 27/05/2023               }
 {                 www.bemoreweb.com.br                  }
 {                     (17)98169-5336                    }
 {                        2003/2023                      }
@@ -15,17 +15,17 @@ type
   iEntidadeContato<T> = interface
     ['{192E7E0C-030F-41A8-9D49-63A8D4682736}']
     function Id(Value : Integer)       : iEntidadeContato<T>; overload;
-    function Id                        : Integer;          overload;
+    function Id                        : Integer;             overload;
     function IdPessoa(Value : Integer) : iEntidadeContato<T>; overload;
-    function IdPessoa                  : Integer;          overload;
+    function IdPessoa                  : Integer;             overload;
     function Email(Value : String)     : iEntidadeContato<T>; overload;
-    function Email                     : String;           overload;
+    function Email                     : String;              overload;
     function DD(Value : String)        : iEntidadeContato<T>; overload;
-    function DD                        : String;           overload;
+    function DD                        : String;              overload;
     function Telefone(Value : String)  : iEntidadeContato<T>; overload;
-    function Telefone                  : String;           overload;
+    function Telefone                  : String;              overload;
     function Tipo(Value : String)      : iEntidadeContato<T>; overload;
-    function Tipo                      : String;           overload;
+    function Tipo                      : String;              overload;
 
     function &End : T;
   end;
